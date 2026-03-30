d47ca70d7d068822551cbb2d354969cd8fd51654
o.gribov <o.gribov@astondevs.ru>
2026-03-30 18:21:27 +0300

    #3

--- service-ui/src/api/axiosConfig.js
+++ service-ui/src/api/axiosConfig.js
@@ -8,6 +8,7 @@ const api = axios.create(
 
 api.interceptors.request.use(
     (config) => {
+        localStorage.removeItem("token")
         const token = localStorage.getItem("token");
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
