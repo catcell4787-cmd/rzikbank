88a9da49692a559ec81be5d541bba51bb452f751
grbm <oleg4787@yandex.ru>
2026-08-07 12:19:52 +0300

    #2 добавил строчку привет

--- service-config/src/main/java/org/bank/serviceconfig/ServiceConfigApplication.java
+++ service-config/src/main/java/org/bank/serviceconfig/ServiceConfigApplication.java
@@ -12,6 +12,7 @@ public class ServiceConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceConfigApplication.class, args);
+        System.out.println("Привет");
     }
 
 }
